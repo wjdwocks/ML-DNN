7001a6b2006c8d7a92d7f94dbb83600b4f26b5f4
jaechan <wocks1626@gmail.com>
2025-02-25 14:35:14 +0900

    25.2.25

--- ML/pytorch/Yolo/coco_dataset.py
+++ ML/pytorch/Yolo/coco_dataset.py
@@ -8,7 +8,7 @@ torch.cuda.set_device(1)
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 # YOLOv8 모델 로드 (Nano 버전)
-model = YOLO("yolov8n.pt")
+model = YOLO("runs/detect/train27/weights/best.pt")
 model.to(device)
 
 # 모델 정보 출력
@@ -17,7 +17,7 @@ print('---' * 20)
 summary(model.model, input_size=(1, 3, 640, 640), col_names=['input_size', 'output_size', 'num_params'])
 
 # COCO 데이터셋으로 학습 시작
-model.train(data="coco.yaml", epochs=100, patience = 10, lr0 = 0.01, batch=32, imgsz=640, device = "cuda:1",  weight_decay=0.001, dropout=0.1, mosaic=0.5, mixup=0.2)
+model.train(data="coco.yaml", epochs=100, patience = 50, lr0 = 0.01, batch=32, imgsz=640, device = "cuda:1",  weight_decay=0.001, dropout=0.1, mosaic=0.5, mixup=0.2)
 
 # 모델 평가 (Validation 데이터 사용)
 metrics = model.val(data="coco.yaml", save_json=True, shuffle=True)
